3fb8df44bed9b8a01f2909f35ef2a5caa57859e2
Chad Milburn <cmilburn@ballstate.bsu.edu>
2021-03-09 18:27:35 -0500

    working on scrape functions

--- scrape_mars.py
+++ scrape_mars.py
@@ -12,12 +12,13 @@ import pymongo
 def init_browser():
     executable_path = {'executable_path': ChromeDriverManager().install()}
     browser = Browser('chrome', **executable_path, headless=False)
+    return browser
 
 # create scraping function
 def scrape():
     #splinter
     browser = init_browser()
-    # create dict for HTML rendering
+    # create empty dict for HTML rendering
     mars_dict = {}
 
     # scrape headlines and teaser from news https://mars.nasa.gov/news/ 
@@ -32,5 +33,47 @@ def scrape():
     news_title = news_soup.find_all('div', class_='content_title')[0].text.strip()
     news_p = news_soup.find_all('div', class_='rollover_description_inner')[0].text.strip()
 
-    return
-    print(news_title)
+    # JPL Image 
+    
+    jpl_url = 'https://data-class-jpl-space.s3.amazonaws.com/JPL_Space/index.html'
+    browser.visit(jpl_url)
+    time.sleep(3)
+    html = browser.html
+    jpl_image_soup = bs(html, 'html.parser')
+
+    # scrape the needed link and concatnate with link for full image link
+    image = jpl_image_soup.find('a', class_='showimg')['href']
+    featured_image_url = 'https://data-class-jpl-space.s3.amazonaws.com/JPL_Space/' + image
+
+    # Mars Facts-Scrape table and convert to HTML
+    tableurl = "https://space-facts.com/mars/"
+    tables = pd.read_html(tableurl)
+    df = tables[0]
+    html_table = df.to_html(index=False, header=False)
+
+    # Mars Hemispheres
+    # set up urls 
+    # this url is the base to use to find both the name and image info
+    base_url = 'https://astrogeology.usgs.gov'
+    # this addition to the base will allow us to scrape the hemisphere name
+    url = '/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
+    browser.visit(base_url + url)
+    time.sleep(3)
+    facts_soup = bs(html, 'html.parser')
+    names = facts_soup.find_all('div', class_='item')
+    # loop to get and store names in a list
+    titles=[]
+    for name in names:
+        titles.append(name.find('h3').text.strip())
+    # loop the get and store hemisphere urls to get image sub url. This is loacted in a element and must be concatenated
+    # to the base url
+    title_url = []
+    for name in names:
+        title_url.append(base_url + (name.find('a')['href']))
+
+    
+
+
+
+
+
